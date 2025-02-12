5cadde1a88005da8ba8eba47d5fc3f3400d448f0
jlopes <joao.victor@lojainterativa.com>
2025-02-12 16:08:14 -0300

    🆕 feat(Exemplos/Timer): Adiciona componente Timer e integra ao Exemplos

--- src/Exemplos.js
+++ src/Exemplos.js
@@ -2,12 +2,19 @@ import React, {useEffect, useState} from "react";
 import InputName from "./components/InputNome";
 import Counter from "./components/Counter";
 import UserCard from "./components/UserCard";
+import Timer from "./components/Timer";
 
 const Exemplos = () => {
 
     const [users, setUser] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+
+    const [isRunning, setIsRunning] = useState(false);
+
+    const handleTimer = (run) => {
+        setIsRunning(run);
+    };
 
     useEffect(() => {
         fetch("https://api.npoint.io/04c44b2a36ab58dfebfc")
@@ -30,6 +37,18 @@ const Exemplos = () => {
 
     return (
         <>
+            <Timer isRunning={isRunning}/>
+            <div className="text-center mb-4">
+                <button
+                    className="btn btn-success me-2"
+                    onClick={() => handleTimer(true)}
+                >
+                    Start
+                </button>
+                <button className="btn btn-danger" onClick={() => handleTimer(false)}>
+                    Stop
+                </button>
+            </div>
             <InputName/>
             <Counter/>
             {error ? ( // Se houver erro, exibe a mensagem de erro
